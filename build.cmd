c0e34d2fbc9cce0761f5101c73e8ec7bb07884a8
Sky Morey <smorey@degdigital.com>
2020-03-21 21:45:55 -0500

    updates

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
-rmdir /S /Q _vcpkg\buildtrees\quickbms\src\0.10.1-6672693ee9\_build
-mkdir _vcpkg\buildtrees\quickbms\src\0.10.1-6672693ee9\_build & pushd _vcpkg\buildtrees\quickbms\src\0.10.1-6672693ee9\_build
+rmdir /S /Q _vcpkg\buildtrees\quickbms\src\0.10.1-615638c868\_build
+mkdir _vcpkg\buildtrees\quickbms\src\0.10.1-615638c868\_build & pushd _vcpkg\buildtrees\quickbms\src\0.10.1-615638c868\_build
 cmake -G "Ninja" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ ..
 popd
-cmake --build _vcpkg\buildtrees\quickbms\src\0.10.1-6672693ee9\_build
+cmake --build _vcpkg\buildtrees\quickbms\src\0.10.1-615638c868\_build
